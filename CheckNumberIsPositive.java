8c41d152b5fc2d0ad6981d757b0a4662abe46a92
SaranyaShanuN <shanu26@gmail.com>
2024-10-18 21:48:04 +0530

    Add files via upload
    Week1

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,15 @@
+package week1.homeassignment;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		int version =10;
+		if (version > 0) {
+			System.out.println("Version " +version+" is positive");
+		}
+			else {
+				System.out.println("Version " +version+" is Negative");	
+		}
+	}
+
+}
---
+++ LibraryManagement.java
@@ -0,0 +1,12 @@
+package week1.homeassignment;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		Library b = new Library();
+		b.addBook("Selenium");
+		b.issueBook();
+		
+	}
+
+}
